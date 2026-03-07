709c707e2890b312b7fa264df1a72fd5ba23d796
krishna <krishnacodes17@gmail.com>
2026-03-07 19:20:22 +0530

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from 'gsap'
+import React from 'react'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+function App() {
+  return (
+    <main>
+        <h1>helow ji</h1>
+    </main>
+  )
+}
+
+export default App
