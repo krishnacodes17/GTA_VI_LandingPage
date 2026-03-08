bbd7c6f18376a048cc6e96d1748cb01833017305
krishna <krishnacodes17@gmail.com>
2026-03-08 10:54:53 +0530

    Postcard section completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/secondVideo";
 import Lucia from "./sections/Lucia";
+import PostCard from "./sections/PostCard";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -20,7 +21,7 @@ function App() {
     <SecondVideo />
     <Lucia />
 
-
+  <PostCard />
 
 
   </main>;
